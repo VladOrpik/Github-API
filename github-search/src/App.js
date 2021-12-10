a47381061e703839f76b9cf7845365083fa4ab07
VladOrpik <65450885+VladOrpik@users.noreply.github.com>
2021-12-10 17:37:29 +0300

    add filter

--- github-search/src/App.js
+++ github-search/src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
   const [inputValue, setinputValue] = React.useState("");
   const [repos, setRepos]= React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
+  const [userNameQuery, setUserNamrQuery]= React.useState(null);
   const token = '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';
- React.useEffect(()=>{
-   if(!inputValue){
-     return;
-   }
-   
-   setIsLoading(true);
+  let url = 'https://api.github.com/search/repositories?q';
 
-   fetch('https://api.github.com/search/repositories?q='+ inputValue, {
+function Filter(){
+   fetch(`https://api.github.com/search/repositories?q=${inputValue}${userNameQuery? ' user:' + userNameQuery:""}`, {
     headers: {
       Authorization: `token ${token}`
     }
@@ -25,35 +24,55 @@ function App() {
     console.log(data);
     setIsLoading(false);
     setRepos(data.items);
-  });
+  })
+  .catch(err=>{
+    console.log(err)
+    setIsLoading(false);
+    setError(true);
+  }) 
+  return;
+}
+
+ React.useEffect(()=>{
+   if(!inputValue){
+     return;
+   }
+   setIsLoading(true);
+   Filter();
  },[inputValue])
- console.log(repos);
+ 
+ React.useEffect(()=>{
+  Filter();
+
+ },[userNameQuery])
 
   return (
     <div className='app'>
       <h1 align="center">GitHub Search</h1>
       <form onChange={evt=>{
-        evt.preventDefault();
-        setinputValue(evt.target.value);
+      setinputValue(evt.target.value);
       }} >
-      <input className='form__input' type="text" name="query" placeholder="Поиск"/>
+      <input className='form__input' type="text" name="query" placeholder="Введите название репозитория"/>
       </form>
-      
+      <h2 align="center">Фильтрация</h2>
+      <input  className='form__input2' onChange={evt=>{
+      setUserNamrQuery(evt.target.value);
+      }} type='text' name='query' placeholder="Введети полное имя пользователя"/>
       {isLoading ? <div className='loading'>Loading</div> : <ul>
         {repos ? 
         repos.map(repo=>{
           return <li key={repo.id}>
-             <a  href={repo.html_url}> {repo.name}</a>
+             <a href={repo.html_url}> {repo.name}</a>
              <p>{repo.description}</p>
              <p> Количество  = {repo.forks_count}</p>
           </li>
         }): null}
         
       </ul>}
+     
 
-  
     </div>
+    
   );
 }
-
 export default App;
