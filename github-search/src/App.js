6d98a8a856b61dd6bce4618886864ba118f1a8bf
VladOrpik <65450885+VladOrpik@users.noreply.github.com>
2021-12-10 10:39:50 +0300

    adding react hooks to change the request state

--- github-search/src/App.js
+++ github-search/src/App.js
@@ -1,11 +1,20 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setinputValue] = React.useState("")
+  console.log(inputValue)
+ React.useEffect(()=>{
+   if(!inputValue){
+     console.log("введите запрос")
+   }
+ })
+
   return (
     <div className='app'>
       <form onSubmit={evt=>{
-        console.log("onSubmit")
+        evt.preventDefault();
+        setinputValue(evt.target.elements.query.value);
       }} >
       <input className='form__input' type="text" name="query" placeholder="Поиск"/>
       </form>
